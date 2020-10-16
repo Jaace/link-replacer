aa5bd038641e879df492bd86046050f0513e7623
Jason <jason.boyle@nytimes.com>
2020-10-16 14:03:02 -0400

    More things

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
-console.log('Background.js is firing');
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+    new chrome.declarativeContent.ShowPageAction()
+  });
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  new chrome.declarativeContent.ShowPageAction()
+  // chrome.declarativeContent.onPageChanged.addRules([{
+  //   conditions: [new chrome.declarativeContent.PageStateMatcher({
+  //     pageUrl: {hostEquals: 'developer.chrome.com'},
+  //   })
+  //   ],
+  //       actions: [new chrome.declarativeContent.ShowPageAction()]
+  // }]);
+});
